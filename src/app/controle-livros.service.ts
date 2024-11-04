12a79242e0b19e64de2bf831bd8d47fe59f94f04
thais-mariano <thaismariano89@gmail.com>
2024-11-04 20:23:09 -0300

    feat: add controle-livros.service.ts

--- src/app/controle-livros.service.ts
+++ src/app/controle-livros.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Livro } from './livro';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ControleLivrosService {
+  private livros: Array<Livro> = [
+    { codigo: 1, codEditora: 1, titulo: 'Livro 1', resumo: 'Resumo do Livro 1', autores: ['Autor A'] },
+    { codigo: 2, codEditora: 2, titulo: 'Livro 2', resumo: 'Resumo do Livro 2', autores: ['Autor B', 'Autor C'] },
+    { codigo: 3, codEditora: 3, titulo: 'Livro 3', resumo: 'Resumo do Livro 3', autores: ['Autor D'] }
+  ];
 
-  constructor() { }
+  obterLivros(): Array<Livro> {
+    return this.livros;
+  }
+
+  incluir(novoLivro: Livro): void {
+    const maxCodigo = Math.max(...this.livros.map(l => l.codigo), 0);
+    novoLivro.codigo = maxCodigo + 1;
+    this.livros.push(novoLivro);
+  }
+
+  excluir(codigo: number): void {
+    const indice = this.livros.findIndex(l => l.codigo === codigo);
+    if (indice !== -1) {
+      this.livros.splice(indice, 1);
+    }
+  }
 }
